4a5b8b4cbe71024bd7a8e0f11b1f9cc2ba5d4d24
Axel Sparkster <axelsparkster@gmail.com>
2025-04-10 12:10:34 -0500

    dummy commit

--- main.py
+++ main.py
@@ -7,6 +7,6 @@ if __name__ == '__main__':
     # This line is needed since asyncio by itself has trouble running main() due to the event
     # listener being too busy and causes a "asyncio.run() cannot be called from a running event loop"
     # error.
-    nest_asyncio.apply()
+    nest_asyncio.apply() 
 
     asyncio.run(main())
