42ea823cde807f57e42a2757acf80cc7c41d5b2e
Axel Sparkster <axelsparkster@gmail.com>
2026-04-01 08:24:05 -0500

    fix bug where admins can't reset

--- bot/utils/permissionutils.py
+++ bot/utils/permissionutils.py
@@ -1,2 +1,2 @@
 async def is_owner_or_admin(ctx) -> bool:
-    return await ctx.bot.is_owner(ctx.author) or await ctx.permissions.administrator
+    return await ctx.bot.is_owner(ctx.author) or ctx.permissions.administrator
